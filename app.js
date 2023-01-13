37874514ed24e7d653d096cc8d85325843cab865
Carlos Vitor Duarte <84823862+CV1tor@users.noreply.github.com>
2023-01-13 13:04:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
 .then(response => response.json())
 .then(data => {
     const pokeArray = data.results;
+    pokeArray.sort();
 
     pokeArray.forEach(pokemon => {
         fetch(pokemon.url)
